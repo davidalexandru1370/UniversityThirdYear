dede3ebb0ef979d7fb577539522f650d8a4f6a67
David Alexandru <fanefaraon696@gmail.com>
2023-12-30 15:02:54 +0200

    added: mpi init

--- PPD/Colectiv/src/main/java/org/example/Main.java
+++ PPD/Colectiv/src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import mpi.MPI;
 
 public class Main {
     public static void main(String[] args) {
+        MPI.Init(args);
         int me = MPI.COMM_WORLD.Rank();
         int size = MPI.COMM_WORLD.Size();
 
