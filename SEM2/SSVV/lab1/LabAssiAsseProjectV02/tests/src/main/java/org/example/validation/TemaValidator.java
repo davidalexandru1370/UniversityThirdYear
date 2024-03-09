1a0149ea9661f706c1b42646cf16163cf1b62957
David Alexandru <fanefaraon696@gmail.com>
2024-03-09 22:37:52 +0200

    added: ssvv lab1 hw

--- SEM2/SSVV/lab1/LabAssiAsseProjectV02/tests/src/main/java/org/example/validation/TemaValidator.java
+++ SEM2/SSVV/lab1/LabAssiAsseProjectV02/tests/src/main/java/org/example/validation/TemaValidator.java
@@ -17,10 +17,10 @@ public class TemaValidator implements Validator<Tema> {
         if(entity.getDescriere().equals("")){
             throw new ValidationException("Descriere invalida!");
         }
-        if(entity.getDeadline() < 1 || entity.getDeadline() > 14) {
+        if(entity.getDeadline() < 1 || entity.getDeadline() > 28) {
             throw new ValidationException("Deadlineul trebuie sa fie intre 1-14.");
         }
-        if(entity.getPrimire() < 1 || entity.getPrimire() > 14) {
+        if(entity.getPrimire() < 1 || entity.getPrimire() > 28) {
             throw new ValidationException("Saptamana primirii trebuie sa fie intre 1-14.");
         }
     }
